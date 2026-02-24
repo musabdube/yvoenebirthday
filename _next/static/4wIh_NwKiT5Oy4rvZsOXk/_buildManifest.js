e280be19d5e62d2bf86ed00b6a816b3ddcef2f1b
musabdube <musabdube@gmail.com>
2026-02-25 00:39:19 +0200

    Updates

--- _next/static/4wIh_NwKiT5Oy4rvZsOXk/_buildManifest.js
+++ _next/static/4wIh_NwKiT5Oy4rvZsOXk/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/yvoenebirthday//_next/:path+",
+        "destination": "/yvoenebirthday/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
